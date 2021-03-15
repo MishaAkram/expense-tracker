e5fa5f17ec3c0f6a6f68f4f705a052848c395c6d
MishaAkram <mishaakrambaig@gmail.com>
2021-03-15 23:26:53 +0500

    Tramsactions added to TransactionHistory

---
+++ src/components/Transactions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Transactions = ({transaction}) => {
+    return (
+        <li className="plus">
+                    {transaction.description}
+                    <span>{transaction.transactionAmount}</span>
+                    <button className="delete-btn">X</button>
+                </li>
+    )
+}
